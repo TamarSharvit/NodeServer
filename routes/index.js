f55b408727524b0f5eec58ccc7c9eb02225720c7
TamarSharvit <tamar92932@gmail.com>
2021-06-07 00:30:17 +0300

    login

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 const jwt = require("jsonwebtoken");
 
 var config = {
-  user: 'MBYDOMAIN/322368507',
+   user: '',//'MBYDOMAIN/322368507',
   password: '',
-  server: 'srv2\pupils',
-  database: 'SIUR',
+  server: 'NAYAD-1',
+  database: 'SIURMOCHOT',
   options: {
-    port: 0 //////// ask elkarif
+    // port: 0 //////// ask elkarif
   }
 };
 
